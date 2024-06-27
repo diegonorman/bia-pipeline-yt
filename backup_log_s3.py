b8f0b506ef827402ac12d433e0fd5e349e3798c6
Diego Norman <38059163+diegonorman@users.noreply.github.com>
2024-06-27 13:15:05 -0300

    Update backup_log_s3.py

--- backup_log_s3.py
+++ backup_log_s3.py
@@ -15,6 +15,12 @@ LOG_FILE = "/var/log/backup_log_s3.log"
 # Configurar logging
 logging.basicConfig(filename=LOG_FILE, level=logging.INFO, format='%(asctime)s - %(message)s')
 
+def log_message(message):
+    logging.info(message)
+    print(message)
+
+log_message("Iniciando backup e envio para o S3")
+
 # Obter a data atual no formato YYYY-MM-DD-HH-MM
 CURRENT_DATE = datetime.now().strftime("%Y-%m-%d-%H-%M")
 
@@ -23,12 +29,6 @@ HASH = str(uuid.uuid4())
 
 # Nome do arquivo tgz com data, hora, minuto e hash
 TGZ_FILENAME = f"{DESTINATION_FOLDER}{CURRENT_DATE}-{HASH}.tgz"
-
-def log_message(message):
-    logging.info(message)
-    print(message)
-
-log_message("Iniciando backup e envio para o S3")
 
 # Comprimir a pasta em formato tgz com alta prioridade
 try:
@@ -55,14 +55,14 @@ try:
             if os.path.getmtime(file_path) < cutoff_date.timestamp():
                 os.remove(file_path)
 
-    # Calcular o tamanho do arquivo
+    # Calcular o tamanho do arquivo enviado
     response = s3_client.head_object(Bucket=BUCKET_NAME, Key=os.path.basename(TGZ_FILENAME))
     file_size = response['ContentLength']
 
-    # Calcular o checksum do arquivo
+    # Calcular o checksum do arquivo enviado
     file_checksum = response['ETag'].strip('"')
 
-    log_message(f"Tamanho do arquivo: {file_size} bytes.")
+    log_message(f"Tamanho do arquivo enviado: {file_size} bytes.")
     log_message(f"Checksum do arquivo no S3: {file_checksum}.")
 except Exception as e:
     log_message(f"Erro ao enviar o arquivo para o S3 na região {DESTINATION_REGION}. O arquivo não será excluído: {TGZ_FILENAME}. Erro: {str(e)}")
